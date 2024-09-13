cbd1378b12d57de3660b786b01b69e8db04ad040
Oleg Sechovcov <52458016+GreyZar37@users.noreply.github.com>
2024-09-13 11:42:05 +0200

    New

--- SDU/Training/Clicker Game 12-09-2024/index.js
+++ SDU/Training/Clicker Game 12-09-2024/index.js
@@ -5,8 +5,16 @@ let clickerCost = document.querySelector('.clicker-cost')
 let parsedClickerCost = parseFloat(clickerCost.innerHTML)
 
 
+var timer = window.setInterval(IncrementBananaAuto, 500);
+
 function IncrementBanana(){
     parsedBanana += 1;
+    banana.innerHTML = parsedBanana;
+    
+}
+
+function IncrementBananaAuto(){
+    parsedBanana += 1; 
     banana.innerHTML = parsedBanana;
 }
 
@@ -14,6 +22,48 @@ function BuyClicker(){
     if(parsedBanana >=parsedClickerCost){
         parsedBanana-= parsedClickerCost;
         banana.innerHTML = parsedBanana;
+        bananaAutoPrS += 1
     }
-
 }
+
+
+
+class ClickerArray{
+    constructor(){
+       this.clickers = [];
+    }
+    AddClicker(clickerObj){
+        this.clickers.push(clickerObj)
+    }
+    GetClickers(){
+        return this.clickers;
+    }
+}
+
+clickerArray = new ClickerArray()
+
+
+
+class Clicker{
+    constructor(bananaPrS, upgradeName, upgradeCost){
+        this.bananaPrS = bananaPrS;
+        this.upgradeName = upgradeName;
+        this.upgradeCost = upgradeCost;
+    }
+    GetName() {
+        return this.upgradeName
+    }
+    GetPrice(){
+        return this.price
+    }
+    GetBananaPrS(){
+        return this.bananaPrS
+    }
+}
+
+function CreateClicker(bananaPrS, upgradeName, upgradeCost){
+    let monke = new Clicker(bananaPrS, upgradeName, upgradeCost);
+    clickerArray.AddClicker(monke);
+}
+
+
