15e517dbc531f728e92a93543b55fbf76dc19908
Oleg Sechovcov <52458016+GreyZar37@users.noreply.github.com>
2025-10-20 21:33:54 +0200

    Data

--- SDU/Semester 5/Data struktur og Algoritmer/Portefølje Opgave E2025/PortfolieAlgoritms/PortfolieAlgoritms/Program.cs
+++ SDU/Semester 5/Data struktur og Algoritmer/Portefølje Opgave E2025/PortfolieAlgoritms/PortfolieAlgoritms/Program.cs
@@ -5,7 +5,23 @@ namespace PortfolieAlgoritms
 {
     internal class Program
     {
-       
+        public static void Main(string[] args) {
+            Console.WriteLine(HarTreTalMedSum("82842605"));
+        }
+
+        public static bool HarTreTalMedSum(string stringValue)
+        {
+            for (int i = 0; i < stringValue.Length - 2; i++)
+            {
+                    int a = stringValue[i] - '0';
+                    int b = stringValue[i + 1] - '0';
+                    int c = stringValue[i + 2] - '0';
+
+                    if (c == a + b)
+                        return true;
+            }
+            return false;
+        }
 
     }
 }
