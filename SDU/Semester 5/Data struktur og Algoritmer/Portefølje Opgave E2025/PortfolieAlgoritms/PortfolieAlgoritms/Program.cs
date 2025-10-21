4c5375562f4667d39cbeaf74486263f74b2453a4
Oleg Sechovcov <52458016+GreyZar37@users.noreply.github.com>
2025-10-21 18:34:36 +0200

    algo

--- SDU/Semester 5/Data struktur og Algoritmer/Portefølje Opgave E2025/PortfolieAlgoritms/PortfolieAlgoritms/Program.cs
+++ SDU/Semester 5/Data struktur og Algoritmer/Portefølje Opgave E2025/PortfolieAlgoritms/PortfolieAlgoritms/Program.cs
@@ -5,23 +5,58 @@ namespace PortfolieAlgoritms
 {
     internal class Program
     {
-        public static void Main(string[] args) {
-            Console.WriteLine(HarTreTalMedSum("82842605"));
-        }
-
-        public static bool HarTreTalMedSum(string stringValue)
+        public static void Main(string[] args)
         {
-            for (int i = 0; i < stringValue.Length - 2; i++)
+            int[] numbers = { 24, 56, 22, 11, 65, 89, 3, 44, 87, 910, 45, 35, 98 };
+            var values = SumOfThreeNumbers(numbers);
+            foreach (var value in values)
             {
-                    int a = stringValue[i] - '0';
-                    int b = stringValue[i + 1] - '0';
-                    int c = stringValue[i + 2] - '0';
-
-                    if (c == a + b)
-                        return true;
+                Console.WriteLine(value + "\n");
             }
-            return false;
         }
 
+        private static int[] SumOfThreeNumbers(int[] numbers)
+        {
+            int bestFirstNumber = 0, bestSecondNumber = 0, bestThirdNumber = 0, bestPowerNumber = 0;
+            int bestDiff = int.MaxValue;
+
+            for (int i = 0; i < numbers.Length - 2; i++)
+            {
+                var fNumber = numbers[i];
+                for (int j = i + 1; j < numbers.Length - 1; j++)
+                {
+                    var sNumber = numbers[j];
+                    for (int k = j + 1; k < numbers.Length; k++)
+                    {
+                        var tNumber = numbers[k];
+                        int sum = fNumber + sNumber + tNumber;
+                        int nearestPowerOfTwo = NearestPowerOfTwo(sum);
+                        int difference = Math.Abs(nearestPowerOfTwo - sum);
+
+                        if( difference < bestDiff)
+                        {
+                            bestDiff = difference;
+                            bestFirstNumber = fNumber;
+                            bestSecondNumber = sNumber;
+                            bestThirdNumber = tNumber;
+                            bestPowerNumber = nearestPowerOfTwo;
+                        }
+                    }
+                }
+            }
+            return  new int[] { bestFirstNumber, bestSecondNumber, bestThirdNumber, bestPowerNumber };
+        }
+
+        private static int NearestPowerOfTwo(int number)
+        {
+
+            int power = 1;
+            while (power < number)
+            {
+                power *= 2;
+            }
+
+            return power;
+        }
     }
 }
