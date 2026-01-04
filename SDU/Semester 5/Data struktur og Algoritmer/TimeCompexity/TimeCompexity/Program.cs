5c24491bb4f961a046a2b14c71e2940dab08c59d
Oleg Sechovcov <olegsechovcov@gmail.com>
2026-01-04 14:40:13 +0100

    adu

---
+++ SDU/Semester 5/Data struktur og Algoritmer/TimeCompexity/TimeCompexity/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+static long MyMethod(long N)
+{
+    long x = 0;
+    long y = 0;
+
+    // Outer loop runs in O(log N) time because i is divided by 4 each iteration
+    for (long i = N; i > 0; i=i/4)
+    {
+        // Middle loop runs in O(N) time
+        for (long j = 0; j < N; j++)
+        {
+            // Inner loop runs in O(log(log(n)) time
+            for (long k = 0; k < N*N*N; k++)
+            {
+                x++;
+                k *= k;
+            }
+        }
+    }
+    return x;
+    
+    //The total time complexity will: O(log N) * O(N) * O(log log(N)) =  O(N * log(N) * log log(N))
+}
+Console.WriteLine(MyMethod(16));
